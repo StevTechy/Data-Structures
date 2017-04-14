62501f4f95a541b428825a1242fa0c7898731ed5
Steven Hooper <stevenms@outlook.com>
2017-04-14 21:50:08 +0100

    Addng Double Linked List, likely has issues - review later

---
+++ Data Structures/DoubleLinkedList.cs
@@ -0,0 +1,70 @@
+﻿namespace Data_Structures
+{
+    public class DoubleLinkedList
+    {
+        Node head;
+
+        public void Append(int data)
+        {
+            //If there is no head, initialize the linked list with one
+            if (head == null)
+            {
+                head = new Node(data);
+                return;
+            }
+
+            Node current = head;
+
+            //Keep going through the nodes until we reach the end, then create a new node
+            while (current.Next != null) current = current.Next;
+            current.Next = new Node(data);
+            current.Next.Previous = current;
+        }
+
+        public void Prepend(int data)
+        {
+            //Create a new head, setting the current head to the Next value of the new head
+            Node newHead = new Node(data);
+            newHead.Next = head;
+            head.Previous = newHead;
+            head = newHead;
+        }
+
+        public void DeleteWithValue(int data)
+        {
+            if (head == null) return;
+
+            if (head.Data == data)
+            {
+                head = head.Next;
+                head.Next.Previous = null;
+                return;
+            }
+
+            Node current = head;
+
+            while (current.Next != null)
+            {
+                if (current.Next.Data == data)
+                {
+                    current.Next = current.Next.Next;
+                }
+                current = current.Next;
+            }
+
+            if (current != null) current = null;
+        }
+
+        internal class Node
+        {
+            public Node Next;
+            public Node Previous;
+            public int Data;
+
+            public Node(int data)
+            {
+                this.Data = data;
+            }
+        }
+    }
+}
